bfc4ad82db0a8403e901445099be41f4a00d8ab2
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-03 14:31:45 +0000

    add users/new.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/volunteers'
+    end
+  end
 end
