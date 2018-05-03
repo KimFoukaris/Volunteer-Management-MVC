b581ea36e37625f641a27c0660148f2bf26413be
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-03 15:00:24 +0000

    add post signup to application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
       redirect to '/volunteers'
     end
   end
+
+  post '/signup' do
+      if params[:username] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.create(:username => params[:username], :password => params[:password])
+        session[:user_id] = @user.id
+        redirect '/volunteers'
+      end
+    end
+
 end
