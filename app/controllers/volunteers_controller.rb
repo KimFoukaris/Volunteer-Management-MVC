9840d9afda2d05045fc840f3106acb625720eceb
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-03 15:15:36 +0000

    add get /volunteers and volunteers/index.erb

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,9 @@
 class VolunteersController < ApplicationController
+
+  get "/volunteers" do
+      redirect_if_not_logged_in
+      @volunteers = Volunteer.all
+      erb :'volunteers/index'
+    end
+
 end
