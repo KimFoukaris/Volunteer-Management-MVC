8513ccd7335cf0dafd049ca14b8d3af2b9022168
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-04 05:06:15 +0000

    add logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,13 @@ class UsersController < ApplicationController
       end
     end
 
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
