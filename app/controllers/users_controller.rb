d1ca1599e2167186e2913e895b1dd57b51ffef35
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-04 05:13:19 +0000

    add user login.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,25 @@ class UsersController < ApplicationController
       end
     end
 
+    get '/login' do
+        @error_message = params[:error]
+        if !session[:user_id]
+          erb :'users/login'
+        else
+          redirect '/volunteers'
+        end
+      end
+
+      post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect "/volunteers"
+        else
+          redirect to '/signup'
+        end
+      end
+
   get '/logout' do
     if session[:user_id] != nil
       session.destroy
