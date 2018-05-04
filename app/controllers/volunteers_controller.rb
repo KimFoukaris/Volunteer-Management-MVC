3ab517c306ede74638c3d1ff87dfd81356fd1ca4
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-04 05:02:29 +0000

    add volunteer /post new

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -6,10 +6,20 @@ class VolunteersController < ApplicationController
       erb :'volunteers/index'
     end
 
-    get "/volunteers/new" do
-        redirect_if_not_logged_in
-        @error_message = params[:error]
-        erb :'volunteers/new'
+  get "/volunteers/new" do
+      redirect_if_not_logged_in
+      @error_message = params[:error]
+      erb :'volunteers/new'
+    end
+
+  post "/volunteers" do
+      redirect_if_not_logged_in
+
+      unless Volunteer.valid_params?(params)
+        redirect "/volunteers/new?error=invalid volunteer"
       end
+      Volunteer.create(params)
+      redirect "/volunteers"
+    end
 
 end
