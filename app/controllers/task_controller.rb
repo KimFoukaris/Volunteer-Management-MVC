4f3550596b897854b559559c17e1705a22edb64b
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-04 16:01:55 +0000

    add task edit.erb and controller action

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -12,13 +12,30 @@ class TasksController < ApplicationController
       erb :'tasks/new'
     end
 
-    post "/tasks" do
-        redirect_if_not_logged_in
-        unless Task.valid_params?(params)
-          redirect "/tasks/new?error=invalid task"
-        end
-        Task.create(params)
-        redirect "/tasks"
+  post "/tasks" do
+      redirect_if_not_logged_in
+      unless Task.valid_params?(params)
+        redirect "/tasks/new?error=invalid task"
       end
+      Task.create(params)
+      redirect "/tasks"
+    end
+
+  get "/tasks/:id/edit" do
+      redirect_if_not_logged_in
+      @error_message = params[:error]
+      @task = Task.find(params[:id])
+      erb :'tasks/edit'
+    end
+
+    post "/tasks/:id" do
+      redirect_if_not_logged_in
+      @task = Task.find(params[:id])
+      unless Task.valid_params?(params)
+        redirect "/tasks/#{@task.id}/edit?error=invalid task"
+      end
+      @task.update(params.select{|k|k=="name" || k=="description" || k=="volunteer_id"})
+      redirect "/tasks/#{@task.id}"
+    end
 
 end
