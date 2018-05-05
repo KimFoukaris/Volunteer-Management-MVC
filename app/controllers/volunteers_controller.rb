eba13c9a0e70ec3abd361202b9527429341d341d
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-05 08:14:42 +0000

    reformat task and volunteer show.erb

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -14,7 +14,6 @@ class VolunteersController < ApplicationController
 
   post "/volunteers" do
     redirect_if_not_logged_in
-
     unless Volunteer.valid_params?(params)
       redirect "/volunteers/new?error=invalid volunteer"
     end
